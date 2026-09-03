6db52c887cbe89ba0c8421817d5e9c0f1e4cc96d
Saad Khan ~ (KING) <gitsaad19@gmail.com>
2026-09-03 23:47:13 +0530

    refactored the streaming connections.

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -10,7 +10,8 @@ from app.database import get_db
 from app.dependencies import get_current_user,require_role
 from app.models.job import Job
 from app.schemas.job_schema import JobCreate, JobResponse,JobLogRead
-from app.services.job_service import create_job, get_all_jobs, get_job_alllogs, cancel_job,handle_job_progress,event_stream
+from app.services.job_service import create_job, get_all_jobs, get_job_alllogs, cancel_job,handle_job_progress,event_stream_jobLogs,event_stream_jobStatus
+from app.enums import UserRole
 from uuid import UUID
 from app.enums import UserRole
 
@@ -26,7 +27,7 @@ router = APIRouter(
 
 #------------------------------
 @router.get(
-       "/getjobs",
+       "/getjobs/{page}",
         response_model=list[JobResponse], 
 )
 @limiter.limit(settings.RATE_LIMIT)
@@ -34,8 +35,10 @@ def get_jobs(
     request: Request,
     db: Session = Depends(get_db),
     current_user=Depends(require_role(UserRole.MEMBER)),
+    page: int = 1,
+    page_size: int = 10
 ):
-    return get_all_jobs(db, current_user.id)
+    return get_all_jobs(db, current_user.id, page, page_size)
 #---------------------------------
 @router.post(
     "/createJob"
@@ -87,7 +90,7 @@ def get_job_logs(
 
 @router.post("/canceljobs/{job_id}")
 @limiter.limit(settings.RATE_LIMIT)
-def cancel_job(
+def cancelJob(
     request: Request,
     job_id: UUID,
     db: Session = Depends(get_db),
@@ -109,10 +112,18 @@ async def job_progress_websocket(
         job_id,
     )
 
-@router.get("/jobs/{job_id}/logs/sse")
+@router.get("/logs/sse/{job_id}")
 async def job_logs_sse(job_id: UUID):
 
     return StreamingResponse(
-        event_stream(job_id),
+        event_stream_jobLogs(job_id),
+        media_type="text/event-stream",
+    )
+
+@router.get("/jobstatus/sse/{job_id}")
+async def job_status_sse(job_id: UUID):
+
+    return StreamingResponse(
+        event_stream_jobStatus(job_id),
         media_type="text/event-stream",
     )

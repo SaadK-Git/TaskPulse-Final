9d7d031b572123097c231acae29cb19e519d349d
Saad Khan ~ (KING) <gitsaad19@gmail.com>
2026-09-04 13:32:27 +0530

    Updated Websocket and SSE endpoints to account for cache hit/miss.

--- backend/app/services/job_service.py
+++ backend/app/services/job_service.py
@@ -12,9 +12,9 @@ from app.tasks.image_resize import resize_image
 from app.tasks.report_generation import generate_report
 from app.tasks.scheduled import *
 from app.enums import JobStatus,JobType
-from .cache_service import get_progress_key
+from .cache_service import get_progress_key,get_progress_status_channel,set_progress_status,get_progress_status
 from app.redis_client import redis_client
-from app.database import get_db
+from app.database import SessionLocal
 from uuid import UUID  
 
 from app.services.cache_service import (
@@ -35,8 +35,8 @@ TASK_MAP = {
 
 }
 
-def get_all_jobs(db: Session, user_id: int):
-    return db.query(Job).filter(Job.user_id == user_id).all()
+def get_all_jobs(db: Session, user_id: int,page: int = 1, page_size: int = 10):
+    return db.query(Job).filter(Job.user_id == user_id).offset((page - 1) * page_size).limit(page_size).all()
 
 def create_job(
     db: Session,
@@ -127,11 +127,40 @@ async def handle_job_progress(
     # ----------------------------------------------------------------------------------------
     # Get progress that already exists,if already completed, send it and close the connection
     # ----------------------------------------------------------------------------------------
+    db = SessionLocal()
+    job = db.query(Job).filter(Job.id == job_id).first()
 
+    if job.status in [JobStatus.COMPLETED, JobStatus.FAILED, JobStatus.CANCELLED]:
+        await websocket.send_json(
+            {
+                "job_id": str(job_id),
+                "progress": job.progress,
+            }
+        )
+        await websocket.close()
+        return 
+    #get the current progress and status of the job from Redis
     current_progress = await get_progress(
         job_id
     )
+    #
+    current_status = await get_progress_status(
+        job_id
+    )
+    #Checking because some kind of race condition might have occured,bear with me.
+    if current_status in ["failed", "cancelled","completed"]:
+        await websocket.send_json(
+            {
+                "job_id": str(job_id),
+                "progress": current_progress if current_progress is not None else job.progress
+            }
+        )
+        await websocket.close()
+        return
+    #finally closing the database connection for good.
+    db.close()
 
+    #once we handle all the different post "live" scenarios,we can now imagine being in a "live" scenario where the job is still running.
     if current_progress is not None:
         # Send the current progress to the client
         await websocket.send_json(
@@ -140,43 +169,110 @@ async def handle_job_progress(
                 "progress": current_progress,
             }
         )
-        #if current progress is 100, close the connection
-        if current_progress >= 100:
-            await websocket.close()
-            return
-        
-    #if in a situation when progress is either lost or not yet set.   
-    elif current_progress is None:
-        # fetch the progress from the database and send it to the client whatever it is
-        db = get_db()
-        job = db.query(Job).filter(Job.id == job_id).first()
-        await websocket.send_json(
-                {
-                    "job_id": str(job_id),
-                    "progress": job.progress if job else 0,
-                }
-            )
-        await websocket.close()
-        return
+
     # ---------------------------------------------
     # estabilish pub/sub for new progress
     # ---------------------------------------------
 
     pubsub = create_async_pubsub()
 
-    channel = get_progress_channel(
+    #getting channel keys for progress and status updates.
+    progress_channel = get_progress_channel(
+        job_id
+    )
+    progress_status_channel = get_progress_status_channel(
         job_id
     )
 
+    #Subscribing to the respective channels.
     await pubsub.subscribe(
-        channel
+        progress_channel
+    )
+
+    await pubsub.subscribe(
+        progress_status_channel
     )
 
     # ---------------------------------------------
     # Listen for new progress
     # ---------------------------------------------
+    #Starting the connection with the client and sending the progress updates as they come in.
+    try:
+
+        while True:
+
+            message = await pubsub.get_message(
+                ignore_subscribe_messages=True,
+                timeout=1,
+            )
+
+                
+            if message is not None:
+
+                #Get the name of the channel,which could either be "status" or the "progress" percentage.
+                channel = message["channel"]    
+
+                #Check whether the task has already been failed or cancelled.
+                if channel == progress_status_channel and message["data"] in ["failed", "cancelled"]:
+
+                    #Stop the connection as soon as the task has been failed or cancelled.
+                    await websocket.send_json(
+                        {
+                            "job_id": str(job_id),
+                            "progress": await get_progress(job_id),
+                        }
+                    )
+                    await websocket.close()
+                    break
+
+                #Continue fetching the data if the procedure is not either canceled or failed.
+                progress = int(
+                    message["data"]
+                )
+
+                
+                #Send the progress to the client
+                await websocket.send_json(
+                    {
+                        "job_id": str(job_id),
+                        "progress": progress,
+                    }
+                )
+
+                #If the progress is 100, close the connection since the task has been completed.
+                if progress >= 100:
+                    await websocket.close()
+                    break
+
+    finally:
+
+        #Do final cleanup,unsubscribe from the channels and close the pubsub connection.
+        await pubsub.unsubscribe(
+            progress_channel
+        )
+        await pubsub.aclose()
+
+
+async def event_stream_jobStatus(job_id: UUID):
+    # ----------------------------------------------------------------------------------------
+    # Get progress that already exists,if already completed, send it and close the connection
+    # ----------------------------------------------------------------------------------------
+    db = SessionLocal()
+    job = db.query(Job).filter(Job.id == job_id).first()
+    db.close()
+
+    if job.status in [JobStatus.COMPLETED, JobStatus.FAILED, JobStatus.CANCELLED]:
+        return job.status.value
+            
+    #otherwise continue with the pub/sub for new logs
+    pubsub = create_async_pubsub()
+    
+    #establish connection with pubsub channels for status updates.
+    progress_status_channel = get_progress_status_channel(job_id)
 
     try:
+
+        await pubsub.subscribe(progress_status_channel)
 
         while True:
 
@@ -187,84 +283,80 @@ async def handle_job_progress(
 
             if message is not None:
 
-                progress = int(
-                    message["data"]
+                yield (
+                    f"data: {message['data']}\n\n"
                 )
 
-                await websocket.send_json(
-                    {
-                        "job_id": str(job_id),
-                        "progress": progress,
-                    }
-                )
-
-                if progress >= 100:
-                    await websocket.close()
-                    break
+            if message["data"] in ["failed", "cancelled"]:                 
+                break          
 
     finally:
 
-        await pubsub.unsubscribe(
-            channel
-        )
+        await pubsub.unsubscribe(progress_status_channel)
 
         await pubsub.aclose()
 
-        key = get_progress_key(job_id)
+async def event_stream_jobLogs(job_id: UUID):
+  # ----------------------------------------------------------------------------------------
+    # Get progress that already exists,if already completed, send it and close the connection
+    # ----------------------------------------------------------------------------------------
+    db = SessionLocal()
+    job = db.query(Job).filter(Job.id == job_id).first()
+    db.close()
 
-        redis_client.delete(
-            key
-        )
+    if job.status in [JobStatus.COMPLETED, JobStatus.FAILED, JobStatus.CANCELLED]:
+        return get_job_alllogs(db, job_id)
+            
+    #otherwise continue with the pub/sub for new logs
+    pubsub = create_async_pubsub()
 
+    log_channel = get_log_channel(job_id)
+    status_channel = get_progress_status_channel(job_id)
 
+    try:
 
-async def event_stream(job_id: UUID):
-        
+        # -----------------------------
+        # 1. Historical logs
+        # -----------------------------
 
-        pubsub = create_async_pubsub()
+        logs = await get_logs(job_id)
 
-        channel = get_log_channel(job_id)
+        for log in logs:
 
-        try:
+            yield (
+                f"data: {json.dumps(log)}\n\n"
+            )
 
-            # -----------------------------
-            # 1. Historical logs
-            # -----------------------------
+        # -----------------------------
+        # 2. Subscribe for new logs
+        # -----------------------------
 
-            logs = await get_logs(job_id)
+        await pubsub.subscribe(log_channel)
+        await pubsub.subscribe(status_channel)
 
-            for log in logs:
+        # -----------------------------
+        # 3. Live logs
+        # -----------------------------
+
+        while True:
+
+            message = await pubsub.get_message(
+                ignore_subscribe_messages=True,
+                timeout=1,
+            )
+            if message is not None and message["channel"] == status_channel and message["data"] in ["failed", "cancelled"]:
+                #Stop the stream.
+                break
+
+            if message is not None:
 
                 yield (
-                    f"data: {json.dumps(log)}\n\n"
+                    f"data: {message['data']}\n\n"
                 )
 
-            # -----------------------------
-            # 2. Subscribe for new logs
-            # -----------------------------
+    finally:
 
-            await pubsub.subscribe(channel)
-
-            # -----------------------------
-            # 3. Live logs
-            # -----------------------------
-
-            while True:
-
-                message = await pubsub.get_message(
-                    ignore_subscribe_messages=True,
-                    timeout=1,
-                )
-
-                if message is not None:
-
-                    yield (
-                        f"data: {message['data']}\n\n"
-                    )
-
-        finally:
-
-            await pubsub.unsubscribe(channel)
-
-            await pubsub.aclose()
+        await pubsub.unsubscribe(log_channel)
+        await pubsub.unsubscribe(status_channel)
+        await pubsub.aclose()
 
